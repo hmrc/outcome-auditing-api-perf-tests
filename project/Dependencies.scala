cfb2f6742d1e61b7bfa0600b687dbc128ee83118
Usman Mehmood <125829801+usmn-m@users.noreply.github.com>
2025-07-15 16:07:28 +0100

    removed com.typesafe
    Is already pulled from https://github.com/hmrc/performance-test-runner/blob/main/project/Dependencies.scala#L10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,6 @@ object Dependencies {
   private val gatlingVersion = "3.5.1"
 
   val test = Seq(
-    "com.typesafe"          % "config"                    % "1.4.3"         % Test,
     "uk.gov.hmrc"          %% "performance-test-runner"   % "6.1.0"         % Test
   )
 }
