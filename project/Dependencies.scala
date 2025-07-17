afa56083cef91553332053829525104815fd9751
grahampaulcook <11269301+grahampaulcook@users.noreply.github.com>
2025-07-17 13:46:00 +0100

    CIR-2181 platops-pr-bot

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,6 +5,6 @@ object Dependencies {
   private val gatlingVersion = "3.5.1"
 
   val test = Seq(
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.1.0"         % Test
+    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.2.0"         % Test
   )
 }
