8142d0665230432728412c5afe0c88b8f7f9d5fc
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2025-08-20 16:49:16 +0100

    [CIR-2298] 6.2.0 upgrade

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,9 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  private val gatlingVersion = "3.5.1"
-
-  val test = Seq(
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.1.0"         % Test
-  )
+  val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc" %% "performance-test-runner" % "6.2.0"
+  ).map(_ % Test)
 }
